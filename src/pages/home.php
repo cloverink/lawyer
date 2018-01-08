e4e668ba85dcedf4db6519c7e77a7eae058a4b6e
cloverink <cloverink@gmail.com>
2018-01-08 22:52:52 +0700

    home

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,2 +1,14 @@
+<section class="home-section">
+  <div><img src="/images/1x1.png"></div>
+  <div><img src="/images/1x1.png"></div>
+  <div>
+    ปรึกษาคดีความ<br>
+    โดยทนายผู้เชี่ยวชาญ
+  </div>
+  <div><img src="/images/1x1.png"></div>
+  <div><img src="/images/1x1.png"></div>
+</section>
 
-asdf
+<div class="dvFooter">
+  <a id="lnkConsule" href="/consult" class="btn btn-primary btn-lg">ปรึกษาทนาย</a>
+</div>
