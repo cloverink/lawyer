e2490e0c490811c9fb5cc6dbc8cafb2518d0edf4
cloverink <cloverink@gmail.com>
2018-01-09 02:52:52 +0700

    profile page

---
+++ src/pages/profile.php
@@ -0,0 +1,42 @@
+<?php
+
+
+if(!isLogon()) {
+  header("Location: /");
+  exit();
+}
+
+$user = $_SESSION["user"];
+
+// var_dump($user);
+
+$sql = "select * from user_type where id = " . $user["type"];
+$res = $conn->query($sql);
+$type = $res->fetch_assoc();
+
+?>
+<div class="profile-container">
+
+  <h3>Profile</h3>
+  <div class="profile-body">
+    <div class="avt" style="background-image: url('/<?=$user["avt"]?>')"></div>
+    <div class="desc">
+      <div>Name: <span><?=$user["name"]?></span></div>
+      <div>Email: <span><?=$user["email"]?></span></div>
+      <div>Tel: <span><?=$user["tel"]?></span></div>
+      <div>Type: <span><?=$type["name"]?></span></div>
+      <br>
+      <div>Coin: <span><?=$user["coin"]?></span> บาท</div>
+    </div>
+  </div>
+
+  <div class="profile-footer">
+    <button type="button" class="btn btn-primary btn-lg">ประวัติการรักษา</button>
+  </div>
+  <div class="profile-footer">
+    <button type="button" class="btn btn-primary btn-lg">การชำระเงิน</button>
+  </div>
+  <div class="profile-footer">
+    <a class="btn btn-default" href="/logout" role="button">ออกจากระบบ</a>
+  </div>
+</div>
--- src/partials/header.php
+++ src/partials/header.php
@@ -1,17 +1,25 @@
+<?php
+
+function getActiveMenu($name) {
+  global $template_name;
+  return ($name == $template_name)? "actived" : "";
+}
+
+?>
 <header>
 <a id="lnkHome" href="/"></a>
 <nav>
-  <a href="/consult">ปรึกษาทนาย</a>
-  <a href="/lawyer">ทนายที่ปรึกษา</a>
-  <a href="/about">เกี่ยวกับเรา</a>
-  <a href="/faq">ถามตอบ</a>
-
+  <a href="/consult" class="<?=getActiveMenu("consult")?>">ปรึกษาทนาย</a>
+  <a href="/lawyer" class="<?=getActiveMenu("lawyer")?>">ทนายที่ปรึกษา</a>
+  <a href="/about" class="<?=getActiveMenu("about")?>">เกี่ยวกับเรา</a>
+  <a href="/faq" class="<?=getActiveMenu("faq")?>">ถามตอบ</a>
 
   <?php
     if(isLogon()) {
-      echo '<a href="/profile">ข้อมูลส่วนตัว</a>';
+      
+      echo '<a href="/profile" class="'. getActiveMenu("profile") .'">ข้อมูลส่วนตัว</a>';
     } else {
-      echo '<a href="/login">เข้าสู่ระบบ</a>';
+      echo '<a href="/login" class="'. getActiveMenu("login") .'">เข้าสู่ระบบ</a>';
     }
   ?>
   
