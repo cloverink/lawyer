eec86768affa7da8bfafe834313cb2be63c70ce8
cloverink <cloverink@gmail.com>
2018-02-09 01:55:17 +0700

    chart count booking

--- src/pages/admin.php
+++ src/pages/admin.php
@@ -44,11 +44,17 @@ $sql = "select * from user";
 $res = $conn->query($sql);
 ?>
 
-
-<div class="" style="width: 50%; height: 300px;">
-<canvas id="myChart" ></canvas>
+<div class="" style="width: 100%; max-width: 1200px; margin: 0 auto;">
+  <div class="" style="width: 49.5%; min-height: 200px; display: inline-block;">
+    <canvas id="myChartUserCount" ></canvas>
+  </div>
+  <div class="" style="width: 49.5%; min-height: 200px; display: inline-block;">  
+    <canvas id="myChartPaymentCount" ></canvas>
+  </div>
+  <div class="" style="width: 49.5%; min-height: 200px; display: inline-block;">  
+    <canvas id="myChartBookCount" ></canvas>
+  </div>
 </div>
-
 
 
 
@@ -145,6 +151,32 @@ endif;
 <script>
 
 <?php
+//count booking 
+$book_count_date = array();
+$book_count_cnt = array();
+$sql = "select count(hour) as cnt, DATE(book) dt from book group by DATE(book) order by dt";
+$res = $conn->query($sql);
+while($row = $res->fetch_assoc()) {
+  array_push($book_count_date, $row["dt"]);
+  array_push($book_count_cnt, $row["cnt"]);
+}
+
+echo "var book_count_date = " . json_encode($book_count_date) . ";" . PHP_EOL;
+echo "var book_count_cnt = " . json_encode($book_count_cnt) . ";" . PHP_EOL;
+
+
+//count payment
+$payment_count_date = array();
+$payment_count_cnt = array();
+$sql = "select count(*) cnt, DATE(dtcreate) dt from income group by DATE(dtcreate) order by dt";
+$res = $conn->query($sql);
+while($row = $res->fetch_assoc()) {
+  array_push($payment_count_date, $row["dt"]);
+  array_push($payment_count_cnt, $row["cnt"]);
+}
+echo "var payment_count_date = " . json_encode($payment_count_date) . ";" . PHP_EOL;
+echo "var payment_count_cnt = " . json_encode($payment_count_cnt) . ";" . PHP_EOL;
+
 
 //count user
 $user_count_name = array();
@@ -155,7 +187,6 @@ while($row = $res->fetch_assoc()) {
   array_push($user_count_name, $row["name"]);
   array_push($user_count_cnt, $row["cnt"]);
 }
-
 echo "var user_count_name = " . json_encode($user_count_name) . ";" . PHP_EOL;
 echo "var user_count_cnt = " . json_encode($user_count_cnt) . ";" . PHP_EOL;
 
@@ -171,7 +202,8 @@ function wait() {
   }
 
   __DrawUserChart();
-
+  __DrawPaymentChart();
+  __DrawBookChart();
 }
 
 wait();
@@ -183,7 +215,7 @@ function __DrawUserChart() {
     total += +this;
   });
 
-  var ctx = document.getElementById("myChart");
+  var ctx = document.getElementById("myChartUserCount");
     var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
@@ -200,10 +232,104 @@ function __DrawUserChart() {
         }]
       },
       options: {
+        responsive: true,
+        maintainAspectRatio: true,
         scales: {
           yAxes: [{
             ticks: {
               beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+}
+
+function __DrawPaymentChart() {
+  var total = 0;
+  $.each(payment_count_cnt,function() {
+    total += +this;
+  });
+
+  var ctx = document.getElementById("myChartPaymentCount");
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: payment_count_date,
+        datasets: [{
+          label: '# รายรับของระบบ (' + total * 499 + ' บาท)',
+          data: payment_count_cnt,
+          backgroundColor: [
+            'rgba(54, 162, 235, 0.2)'
+          ],
+          borderWidth: 0
+        }]
+      },
+      options: {
+        elements: {
+            line: {
+                tension: 0
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              autoSkip: false,
+              maxRotation: 45,
+              minRotation: 45
+            }
+          }]
+        }
+      }
+    });
+}
+
+function __DrawBookChart() {
+  var total = 0;
+  $.each(payment_count_cnt,function() {
+    total += +this;
+  });
+
+  var ctx = document.getElementById("myChartBookCount");
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: book_count_date,
+        datasets: [{
+          label: '# ยอดการจอง (' + total + ' ชั่วโมง)',
+          data: book_count_cnt,
+          backgroundColor: [
+            'rgba(255, 206, 86, 0.2)'
+          ],
+          borderWidth: 0
+        }]
+      },
+      options: {
+        elements: {
+            line: {
+                tension: 0
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              autoSkip: false,
+              maxRotation: 45,
+              minRotation: 45
             }
           }]
         }
